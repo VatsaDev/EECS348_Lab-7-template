2e964b51560fb0542f674eac9c0821f42b4dbe9e
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2025-03-27 22:48:26 -0500

    Update football_main.c

--- football_main.c
+++ football_main.c
@@ -1,5 +1,22 @@
+#include <stdio.h>
 #include "football.h"
 
 int main(int argc, char *argv[]) {
+    int score;
+    while (1) {
+        printf("Enter 0 or 1 to STOP\n");
+        printf("Enter the NFL score: ");
+        if (scanf("%d", &score) != 1) {
+            printf("Invalid input. Please enter an integer.\n");
+            while (getchar() != '\n'); // Clear input buffer
+            continue;
+        }
+
+        if (score <= 1) {
+            break;
+        }
+
+        print_combinations(score);
+    }
     return 0;
 }
